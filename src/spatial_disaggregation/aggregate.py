f7a5d0595f6bfe052cfc73f059bc7806c655fb06
jnnr <32454596+jnnr@users.noreply.github.com>
2024-05-17 15:22:00 +0200

    Fix raster aggregation

--- src/spatial_disaggregation/aggregate.py
+++ src/spatial_disaggregation/aggregate.py
@@ -1,35 +1,36 @@
 import xarray as xr
 import geopandas as gpd
+import rasterio as rio
 from rasterstats import zonal_stats
+from pathlib import Path
 
 
 def aggregate_raster_to_polygon(
-        raster_data: xr.Dataset|str,
+        raster: str|Path,
         polygons: gpd.GeoSeries|gpd.GeoDataFrame,
-        transform: None
-) -> gpd.GeoDataFrame:
+        stats: str="sum"
+    ) -> gpd.GeoDataFrame:
     r"""
-    Aggregate raster data with spatial units and aggregate the data.
+    Aggregate raster data with spatial units.
     """
-    # Aggregate raster data to spatial units
-    if isinstance(polygons, gpd.GeoSeries):
-        aggregated_data = polygons.to_frame()
-    elif isinstance(polygons, gpd.GeoDataFrame):
-        aggregated_data = polygons.copy()
-    else:
-        raise ValueError("`polygons` should be either a GeoSeries or a GeoDataFrame.")
-    if isinstance(raster_data, str):
-        raise NotImplementedError("Not implemented yet.")
+    with rio.open(raster) as src:
+        affine = src.transform
+        array = src.read(1)
+        
+        polygons_projected = polygons.to_crs(src.crs)
 
-    if hasattr(raster_data, "name"):
-        name_variable = raster_data.name
-    else:
-        name_variable = "value"
-    print(aggregated_data)
-    a = zonal_stats(polygons.geometry, raster_data) #, affine=transform, stats=['mean', 'sum', 'count'])
-    print(a)
-    aggregated_data[name_variable] = a
-    return aggregated_data
+        zs = zonal_stats(polygons_projected, array, affine=affine, stats=stats, geojson_out=True)
+        
+        result_gdf = gpd.GeoDataFrame.from_features(zs)
+
+        if hasattr(raster, "name"):
+            name_variable = raster.name
+        else:
+            name_variable = "value"
+
+    results_gdf = result_gdf.rename(columns={stats: f"{name_variable}_{stats}"})
+
+    return results_gdf
 
 
 def aggregate_point_to_polygon(points: gpd.GeoDataFrame, polygons: gpd.GeoSeries|gpd.GeoDataFrame, aggfunc='sum'):
