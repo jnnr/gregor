b7a1a43cd2fc0ee7ccd1fcb4b04a4c5f6082dddf
Jann Launer <32454596+jnnr@users.noreply.github.com>
2024-05-30 12:00:41 +0200

    Clean up tests

--- test/test_aggregate.py
+++ test/test_aggregate.py
@@ -24,38 +24,26 @@ def points():
     return gpd.read_file("test/_files/points.geojson")
 
 
-def test_agg_tif_2x2(dummy_raster):
+def test_agg_tif_2x2(square_segmentation_2x2):
     agg_raster_poly = aggregate_raster_to_polygon("test/_files/raster.tif", square_segmentation_2x2())
-    agg_raster_poly = pd.DataFrame(agg_raster_poly, index=square_segmentation_2x2().index)
-    agg_raster_poly.index.name = square_segmentation_2x2().index.name
 
     expected = [
         [2.75, 1.],
         [0.75, 2.],
     ]
 
-    assert (np.rot90(agg_raster_poly["value_sum"].to_numpy().reshape(2, 2), k=1) == expected).all()
+    assert (np.rot90(agg_raster_poly["sum"].to_numpy().reshape(2, 2), k=1) == expected).all()
 
 
 def test_agg_array_2x2(square_segmentation_2x2, dummy_raster):
-    # from rasterstats import zonal_stats
-    # import pandas as pd
-    # import numpy as np
-    # agg_raster_poly = zonal_stats(
-    #     square_segmentation_2x2(), dummy_raster().values, affine=dummy_raster().rio.transform(), stats='sum', nodata=-999
-    # )
-    # agg_raster_poly = pd.DataFrame(agg_raster_poly, index=square_segmentation_2x2().index)
-    # agg_raster_poly.index.name = square_segmentation_2x2().index.name
-    agg_raster_poly = aggregate_raster_to_polygon("test/_files/raster.tif", square_segmentation_2x2())
-    agg_raster_poly = pd.DataFrame(agg_raster_poly, index=square_segmentation_2x2().index)
-    agg_raster_poly.index.name = square_segmentation_2x2().index.name
+    agg_raster_poly = aggregate_raster_to_polygon(dummy_raster(), square_segmentation_2x2())
 
     expected = [
         [2.75, 1.],
         [0.75, 2.],
     ]
 
-    assert (np.rot90(agg_raster_poly["value_sum"].to_numpy().reshape(2, 2), k=1) == expected).all()
+    assert (np.rot90(agg_raster_poly["sum"].to_numpy().reshape(2, 2), k=1) == expected).all()
 
 
 def test_agg_tif_3x3(dummy_raster):
@@ -98,7 +86,5 @@ def test_agg_array_3x3(square_segmentation_3x3, dummy_raster):
     # assert (np.rot90(agg_raster_poly["sum"].to_numpy().reshape(4, 4), k=1) == np.array(expected)).all()
 
 if __name__ == "__main__":
-    test_agg_tif_2x2(dummy_raster)
-    test_agg_array_2x2(square_segmentation_2x2, dummy_raster)
-    # test_agg_tif_3x3(dummy_raster)
-    # test_agg_array_3x3(square_segmentation_3x3, dummy_raster)
+    test_agg_tif_3x3(dummy_raster)
+    test_agg_array_3x3(square_segmentation_3x3, dummy_raster)
