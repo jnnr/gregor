ece3b079c15a24c5d36f7e287eeceee28cd85b7d
Jann Launer <32454596+jnnr@users.noreply.github.com>
2025-04-18 22:57:09 +0200

    Correct function

--- docs/examples/disaggregate-guided-to-raster.py
+++ docs/examples/disaggregate-guided-to-raster.py
@@ -28,16 +28,16 @@ capacityfactor_wind_ESP  = rxr.open_rasterio(PATH_DATA / "data/capacityfactor_wi
 # capacity_wind_ESP.to_file(PATH_DATA / "data/capacity_wind_ESP.geojson")
 
 # #%%
-# x = (-10, 4)
+# x = (-10, 4)  
 # y = (35.6, 44)
-# area_potential = area_potential.coarsen(x=100, y=100, boundary="trim").mean()
+# area_potential = area_potential.coarsen(x=10, y=10, boundary="trim").mean()
 # area_potential_ESP = area_potential.rio.clip_box(minx=xlim[0], miny=ylim[0], maxx=xlim[1], maxy=ylim[1])
 # area_potential_ESP.rio.to_raster(PATH_DATA / "data / area_potential_wind_onshore_ESP.tif")
 
 #%% 
 xlim = (-10, 4)
 ylim = (35.6, 44)
-# capacityfactor_wind = capacityfactor_wind.coarsen(x=100, y=100, boundary="trim").mean()
+# capacityfactor_wind = capacityfactor_wind.coarsen(x=10, y=10, boundary="trim").mean()
 # capacityfactor_wind_ESP = capacityfactor_wind.rio.clip_box(minx=xlim[0], miny=ylim[0], maxx=xlim[1], maxy=ylim[1])
 # capacityfactor_wind_ESP.rio.to_raster(PATH_DATA / "data/capacityfactor_wind_ESP.tif")
 # plt.imshow(capacityfactor_wind.mean(dim="time").to_dataarray().squeeze())
@@ -66,6 +66,7 @@ import xarray as xr
 import numpy as np
 import rasterio as rio
 from gregor.disaggregate import get_belongs_to_matrix
+from gregor.aggregate import aggregate_raster_to_polygon
 
 #%%
 def disaggregate_polygon_to_raster_prioritize(
@@ -87,7 +88,16 @@ def disaggregate_polygon_to_raster_prioritize(
     if not len(priority.dims) == 2 and len(limit.dims) == 2:
         raise ValueError("Both `priority` and `limit` need to be 2-dimensional.")
     
+    # check that priority and limit fully align
+    
     # TODO Check that aggregated limit is enough to disaggregate data
+    limit_agg = aggregate_raster_to_polygon(limit, data_polygon.geometry)
+    check = limit_agg[["sum"]].join(data_polygon[column], how="left")
+    check["sufficient"] = check["sum"] > check[column]
+
+    insufficient = check.loc[~check["sufficient"]]
+    if not insufficient.empty:
+        raise ValueError(f"Limit is not sufficient for these geometries: {insufficient}")
 
     # Transform and match data, if necessary.
     _data_polygon = data_polygon.copy()
@@ -122,14 +132,14 @@ def disaggregate_polygon_to_raster_prioritize(
         priority_order = priority_order.dropna("z")
 
         # Order limit by priority_order
-        limit_in_region_ordered = limit.stack(z=("x", "y"))
-        limit_in_region_ordered = limit_in_region_ordered.reindex_like(priority_order)
+        limit_in_region = limit.where(belongs_to == id_region)
+        limit_in_region_ordered = limit_in_region.stack(z=("x", "y")).reindex_like(priority_order)
 
         # Create cumsum of limit
         cumsum = limit_in_region_ordered.cumsum()
 
         # Fill up pixels apart from the last one, where cumsum exceeds data[column]
-        fill_up = cumsum.where(data[column] > cumsum).dropna("z")
+        fill_up = limit_in_region_ordered.where(data[column] > cumsum).dropna("z")
         if not fill_up.isnull().all():
             fill_up_unstack = fill_up.unstack()
             fill_up_unstack.name = name_result
@@ -140,39 +150,42 @@ def disaggregate_polygon_to_raster_prioritize(
             result = xr.merge([result, fill_up_unstack], compat="no_conflicts")
 
         # Find the coordinate where cumsum exceeds data[column]
-        argwhere_last_pixel = np.argwhere(data[column] <= cumsum.values)[0]
-        cumsum_where = cumsum[argwhere_last_pixel].unstack()
-        cumsum_where.name = name_result
-        rest = cumsum_where - data[column]
+        argwhere_last_pixel = np.argwhere(data[column] <= cumsum.values)[0][0]
+        already_filled = limit_in_region_ordered[:argwhere_last_pixel].sum()
+        rest = data[column] - already_filled
 
-        result = xr.merge([result, rest], compat="no_conflicts")
         # Write rest to result at that coordinate
-        # result.sel(x=coords_last_pixel["x"], y=coords_last_pixel["y"])
-        # print(data[column])
-        # print(fill_up)
-        # print(argwhere_last_pixel)
-        # print(cumsum)
-        print("\n")
+        value_last_pixel = limit_in_region_ordered[[argwhere_last_pixel]]
+        value_last_pixel.values = [rest]
+        value_last_pixel.name = name_result
+        value_last_pixel = value_last_pixel.unstack()
+
+        result = xr.merge([result, value_last_pixel], compat="no_conflicts")
 
     return result.to_dataarray()
 
 # We disaggregate to raster
 df = capacity_wind_ESP.copy()
-df["capacity_wind"] *= 0.002
-wind_allocation = disaggregate_polygon_to_raster_prioritize(df, column="capacity_wind", priority=capacityfactor_wind_ESP, limit=area_potential_ESP)
-
+df["capacity_wind"] *= 0.05
+df = df[~df.index.isin([30, 44, 45])]
+i = 1
+wind_allocation = disaggregate_polygon_to_raster_prioritize(synth, column="capacity_wind", priority=capacityfactor_wind_ESP, limit=area_potential_ESP)
+print(wind_allocation.sum())
+print(synth["capacity_wind"].sum())
 #%%
 
 fig, axs = plt.subplots(1, 4, figsize=(12, 3), constrained_layout=True)
 capacity_wind_ESP.plot(ax=axs[0], column="capacity_wind", aspect=None)
 area_potential_ESP.plot(ax=axs[1], aspect=None)
-capacityfactor_wind_ESP.plot(ax=axs[2], cmap="Blues")
+capacityfactor_wind_ESP.plot(ax=axs[2], cmap="inferno", vmax=0.3)
 wind_allocation.plot(ax=axs[3])
+capacity_wind_ESP.geometry.boundary.plot(ax=axs[3], color="k", alpha=0.2, aspect=None)
 
 axs[0].set_title("Modelled wind capacity")
 axs[1].set_title("Area potential")
 axs[2].set_title("Mean wind capacity factor")
 axs[3].set_title("Allocation")
+
 for ax in axs:
     ax.set_xlim(xlim)
     ax.set_ylim(ylim)
@@ -183,7 +196,7 @@ for ax in axs:
 plt.show()
 
 #%%
-print(df["capacity_wind"].sum())
+print(synth["capacity_wind"].sum())
 print(wind_allocation.sum())
 # import numpy as np
 # np.where(wind_allocation.values)
@@ -207,4 +220,9 @@ def test_disaggregate_polygon_to_raster_guided():
     pass
 
 # %%
-df["capacity_wind"]
+capacity_wind_ESP.iloc[30]
+
+synth = aggregate_raster_to_polygon(area_potential_ESP, df.geometry)
+synth["capacity_wind"] = synth["sum"] * 0.99#.apply(lambda x: x * (np.random.rand() * 0.5 +0.5) , 1)
+synth
+# %%
