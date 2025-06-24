e5574e0ef1592a63af31b816c516a28eb817be07
Jann Launer <32454596+jnnr@users.noreply.github.com>
2025-06-24 10:37:36 +0200

    Add alternative implementation for timeseries disaggregation

--- src/gregor/timeseries.py
+++ src/gregor/timeseries.py
@@ -2,7 +2,7 @@ import pandas as pd
 import geopandas as gpd
 import xarray as xr
 from gregor.aggregate import _aggregate_xarray_to_polygon
-from gregor.disaggregate import disaggregate_polygon_to_raster
+from gregor.disaggregate import disaggregate_polygon_to_raster, get_belongs_to_matrix
 
 
 def aggregate_timeseries_raster_to_polygon(
@@ -28,8 +28,8 @@ def aggregate_timeseries_raster_to_polygon(
         DataFrame with the aggregated timeseries per polygon.
     """
     results = {}
-    for t in raster.time.values:
-        raster_t = raster.sel(time=t)
+    for t in raster.t.values:
+        raster_t = raster.sel(t=t)
         results_gdf_t = _aggregate_xarray_to_polygon(raster_t, polygons, stats)
         results_gdf_t = pd.DataFrame(results_gdf_t.drop(columns="geometry"))
         results[t] = results_gdf_t[stats]
@@ -67,7 +67,46 @@ def disaggregate_timeseries_polygon_to_raster(
         rasters.append(raster_t)
 
     # Combine results into a Dataset
-    rasters_ds = xr.concat(rasters, dim="time")
-    rasters_ds = rasters_ds.assign_coords(time=data.index)
+
+    rasters_ds = xr.concat(rasters, dim="t")
+    coords = data.index
+    coords.name = "t"
+    rasters_ds = rasters_ds.assign_coords(t=coords)
     rasters_ds.rio.write_crs(rasters[0].rio.crs)
+
     return rasters_ds
+
+
+def disaggregate_timeseries_polygon_to_raster_2(
+    data: pd.DataFrame,
+    geometries: gpd.GeoDataFrame,
+    column: str,
+    proxy: xr.Dataset,
+    to_data_crs: bool = False,
+) -> xr.Dataset:
+
+    data_sum = data.sum(axis=0)
+    data_sum.name = column
+    data_sum = gpd.GeoDataFrame(data_sum, geometry=geometries.geometry, crs=geometries.crs)
+
+    raster = disaggregate_polygon_to_raster(
+        data_sum,
+        column=column,
+        proxy=proxy,
+        to_data_crs=to_data_crs,
+    )
+
+    # Apply profile
+    raster_xyt = xr.DataArray(data=None, dims=["y", "x", "t"], coords={"y": raster.y, "x": raster.x, "t": data.index})
+    data_t = xr.DataArray(data, dims=['t', "id"], coords={'t': data.index, "id": data.columns})
+    # raster_{x,y,t} = raster{x,y} * belongs_to_{id,x,y} * profile{id,t} / data_sum{id}
+    for id, data_sum_id in data_sum[column].items():
+        raster_id = (
+            raster
+            * data_t.sel(id=id)
+            / data_sum_id
+        )
+        # print(belongs_to == id)
+        raster_xyt.combine_first(raster_id)
+
+    return raster_id
