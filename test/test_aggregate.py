c980775014ff1cdba0441daccbfe65da561f8aeb
jnnr <32454596+jnnr@users.noreply.github.com>
2025-11-14 20:19:04 +0100

    Reduce code duplication by parametrising tests

--- test/test_aggregate.py
+++ test/test_aggregate.py
@@ -10,64 +10,66 @@ from fixtures import (
 )
 
 
-def test_agg_tif_2x2(square_segmentation_2x2):
-    agg_raster_poly = aggregate_raster_to_polygon(
-        "test/_files/raster.tif", square_segmentation_2x2
-    )
-    agg_raster_poly = agg_raster_poly["sum"].to_numpy().reshape(2, 2)
+@pytest.fixture
+def expected_raster_to_2x2():
     expected = [
         [2.75, 1.0],
         [0.75, 2.0],
     ]
-
-    assert_array_equal(agg_raster_poly, expected)
-
-
-def test_agg_array_2x2(square_segmentation_2x2, dummy_raster):
-    agg_raster_poly = aggregate_raster_to_polygon(dummy_raster, square_segmentation_2x2)
-    agg_raster_poly = agg_raster_poly["sum"].to_numpy().reshape(2, 2)
-
-    expected = [
-        [2.75, 1.0],
-        [0.75, 2.0],
-    ]
-
-    assert_array_equal(agg_raster_poly, expected)
+    return expected
 
 
-@pytest.mark.skip(
-    "rasterstats.zonal_stats does provide exact aggregation when pixels are not aligned with polygons. "
-    "This is because a pixel can only belong or not belong to a polygon and not be split."
-)
-def test_agg_tif_3x3(square_segmentation_3x3):
-    agg_raster_poly = aggregate_raster_to_polygon(
-        "test/_files/raster.tif", square_segmentation_3x3
-    )
-    agg_raster_poly =  agg_raster_poly["sum"].to_numpy().reshape(3, 3)
-
+@pytest.fixture
+def expected_raster_to_3x3():
     expected = [
         [2.50, 1.50, 1.00],
         [2.50, 1.75, 1.75],
         [0.75, 0.50, 2.00],
     ]
+    return expected
 
-    assert_array_equal(agg_raster_poly, expected)
 
-
-@pytest.mark.skip(
-    "rasterstats.zonal_stats does provide exact aggregation when pixels are not aligned with polygons. "
-    "This is because a pixel can only belong or not belong to a polygon and not be split."
+@pytest.mark.parametrize(
+    'raster, polygons, expected', 
+    [
+        ('dummy_raster', 'square_segmentation_2x2', "expected_raster_to_2x2"),
+        # ('dummy_raster', 'squares_3x3', "expected_raster_to_3x3"),
+        # rasterstats.zonal_stats does provide exact aggregation when pixels are not aligned with polygons.
+        # This is because a pixel can only belong or not belong to a polygon and not be split.
+    ],
 )
-def test_agg_array_3x3(square_segmentation_3x3, dummy_raster):
-    agg_raster_poly = aggregate_raster_to_polygon(
-        dummy_raster(), square_segmentation_3x3()
+def test_raster_dataarray_to_poly(raster, polygons, expected, request):
+    raster = request.getfixturevalue(raster)
+    polygons = request.getfixturevalue(polygons)
+    expected = request.getfixturevalue(expected)
+
+    agg_raster_poly = aggregate_raster_to_polygon(raster, polygons)
+    
+    expected = (np.array(expected).flatten())
+
+    assert_array_equal(
+        agg_raster_poly["sum"].to_numpy(), 
+        expected
     )
-    agg_raster_poly = agg_raster_poly["sum"].to_numpy().reshape(3, 3)
 
-    expected = [
-        [2.50, 1.50, 1.00],
-        [2.50, 1.75, 1.75],
-        [0.75, 0.50, 2.00],
-    ]
 
-    assert_array_equal(agg_raster_poly, expected)
+@pytest.mark.parametrize(
+    'file, polygons, expected', 
+    [
+        ('test/_files/raster.tif', 'square_segmentation_2x2', "expected_raster_to_2x2"),
+        # ('test/_files/raster.tif', 'squares_3x3', "expected_raster_to_3x3"),
+        # rasterstats.zonal_stats does provide exact aggregation when pixels are not aligned with polygons.
+        # This is because a pixel can only belong or not belong to a polygon and not be split.
+    ],
+)
+def test_raster_file_to_poly(file, polygons, expected, request):
+    polygons = request.getfixturevalue(polygons)
+    expected = request.getfixturevalue(expected)
+    agg_raster_poly = aggregate_raster_to_polygon(file, polygons)
+    
+    expected = (np.array(expected).flatten())
+
+    assert_array_equal(
+        agg_raster_poly["sum"].to_numpy(), 
+        expected
+    )
